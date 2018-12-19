8cee6d00d58eca4a399d4537c113380bf97b1ff6
Mario Salles <mariofsalles@gmail.com>
2018-12-19 21:06:10 +0000

    Condicionais: testes, if, else

---
+++ Modulo1/condicionais.rb
@@ -0,0 +1,23 @@
+puts 5 > 4
+puts 5 < 4
+puts 5 >= 4
+puts 8 == 8
+puts 8 == 7
+puts 7 != 8
+# && e and
+# || e or
+# ! negacao
+num = 5
+if num > 2 && num < 10
+  puts 'ola e'
+end
+
+if num > 7 || num < 10
+  puts 'ola ou'
+end
+
+if 5 >= 3
+  puts 'esse codigo será lido'
+else
+  puts 'esse não'
+end
