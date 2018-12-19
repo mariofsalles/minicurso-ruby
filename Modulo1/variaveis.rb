83aa0d529411a121d48cf3eb92ee06cdad92f1cb
Mario Salles <mariofsalles@gmail.com>
2018-12-19 20:00:40 +0000

    Variaveis

---
+++ Modulo1/variaveis.rb
@@ -0,0 +1,11 @@
+# locais: nao podem ser acessadas por outro metodo
+age = 5 # tipo inteiro
+teste = nil # vazia
+# instancia: sendo declarada dentro de um metodo, pode ser acessada por qualquer objeto da classe
+@num = 2.3 # tipo float
+@name = 'João' # tipo string
+# globais: acessada por todo o programa
+$var = true # tipo booleana
+# constantes
+PI = 3.14
+ARR = [1, 2, 3] # array
