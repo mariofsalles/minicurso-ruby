1d54a990a496e8c3c8c0fdf10b55035dc0ee6bfc
Mario Salles <mariofsalles@gmail.com>
2018-12-19 16:54:22 +0000

    Pasta modulo 1 e primeiro programa

---
+++ Modulo1/meu_primeiro_programa.rb
@@ -0,0 +1,14 @@
+#Imprime na tela
+puts 'Olá mundo'
+puts 5 + 3
+
+#Atribuindo valor a variavel
+nome = 'João'
+cor = "azul"
+#Concatenando variavel
+puts 'Olá ' + nome + '. Bem vindo ao mundo Ruby =D'
+puts nome + 'teste'
+puts "#{nome} teste"
+
+#Fazendo interpolaćão de variável
+puts "Olá #{nome}. Bem vindo ao mundo Ruby =D"
