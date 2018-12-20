2cea0519c702a91ff58a7ea1b1155556fe1a4e1a
Mario Salles <mariofsalles@gmail.com>
2018-12-20 09:42:12 +0000

    Case

---
+++ Modulo1/case.rb
@@ -0,0 +1,16 @@
+puts 'Digite um numero do menu'
+puts '1 - Abrir arquivo'
+puts '2 - Editar arquivo'
+puts '3 - Salvar arquivo'
+menu = gets.chomp.to_i
+
+case menu
+  when 1
+    puts 'Abrir arquivo'
+  when 2
+    puts 'Abrir arquivo'
+  when 3
+    puts 'Abrir arquivo'
+  else
+    puts 'Opção inválida!'
+end
